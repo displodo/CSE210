97c0d789ef9016f723dd2d79418649539df2b753
JR Forsyth <for20002@byui.edu>
2023-01-10 18:13:34 -0700

    Test commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        string grade = Console.ReadLine() ?? "";
+        int NumberGrade = int.Parse(grade);
+        GetGrade(NumberGrade);
     }
+    static void GetGrade(int NumberGrade) {
+        if (NumberGrade >= 90) {
+            Console.WriteLine("You have an A!");
+        }
+        else if (NumberGrade >= 80 && NumberGrade <= 90) {
+            Console.WriteLine("You have an B!");
+        }
+        else if (NumberGrade >= 70 && NumberGrade <= 80) {
+            Console.WriteLine("You have an C!");
+        }
+        else if (NumberGrade >= 60 && NumberGrade <= 70) {
+            Console.WriteLine("You have an D.");
+        }
+        else if (NumberGrade < 60) {
+            Console.WriteLine("You got an F :(");
+        }
+    }
+
 }
