2bbe3e65d1f9201086d2adc8a4c76ec9eb0bd4b8
JR Forsyth <for20002@byui.edu>
2023-01-13 12:02:37 -0700

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Please enter a list of numbers.");
+        Console.WriteLine("When you are finished type 0 to end program.");
+        
+        int input = 1;
+        List<int> numbers = new List<int>();
+
+        do {
+            Console.Write("Please enter a number: ");
+            input = int.Parse(Console.ReadLine() ?? "");
+                       
+            
+            numbers.Add(input);
+
+        } while (input != 0);
+
+        int length = (numbers.Count);
+        int count = -1;
+
+        numbers.Remove(numbers[length - 1]);
+        numbers.Sort();
+        Console.WriteLine(length);
+        Console.Write("Here is your sorted list: ");
+        foreach (int number in numbers) {
+            count += 1;
+            if (numbers[count] != numbers[length - 2]) {
+                Console.Write($"{number}, ");
+            }
+            else {
+                Console.Write(numbers[length - 2]);
+            }
+            
+        }
     }
 }
