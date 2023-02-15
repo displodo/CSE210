b7ec059ef62c313d6e8f7838a285f824c971ed8d
JR Forsyth <for20002@byui.edu>
2023-02-14 21:06:20 -0700

    Assignment Program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        string name = "JR Forsyth";
+        string topic = "Math in the Real World";
+        string writeTopic = "Basic Writing";
+        string textbook = "Chapter 01";
+        string problems = "8-17";
+        string title = "Love at Home by JR Forsyth";
+        var assign = new Assignment(name, topic);
+        var math = new MathAssignment(name, topic, textbook, problems);
+        var writing = new WritingAssignment(name, writeTopic, title);
+        Console.WriteLine("Which would you like to do?");
+        Console.WriteLine("1: Display Student/Assignment.");
+        Console.WriteLine("2: Display Math Assignment.");
+        Console.WriteLine("3: Display Writing Assignment.");
+        Console.Write(">");
+        string answer = Console.ReadLine();
+        Console.WriteLine("");
+        if(answer == "1")
+        {
+            Console.WriteLine(assign.GetSummary());
+            Console.WriteLine("");
+        }
+        if(answer == "2")
+        {
+            Console.WriteLine(math.GetSummary());
+            Console.WriteLine(math.GetHomeworkList());
+            Console.WriteLine();
+        }
+        if(answer == "3")
+        {
+            Console.WriteLine(writing.GetSummary());
+            Console.WriteLine(writing.GetWritingInformation());
+            Console.WriteLine();
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,52 +1,21 @@
 class Program {
     static void Main(string[] args) {
-        
-        var divider =  new IntegerDivision();
-        divider.SetLhsRhs(10, 1);
-        divider.DisplayResult();
-
-        divider.SetLhsRhs(0, 100);
-        divider.DisplayResult();
-
-        divider.SetLhsRhs(5, 0);
-        divider.DisplayResult();
-
-        divider.SetLhsRhs(25, 5);
-        divider.DisplayResult();
-
-
+        Person p = new Person();
+        Student s = new Student();
+        Console.WriteLine(p.GetName());
+        Console.WriteLine(s.GetName());
     }
 }
 
-class IntegerDivision {
-    private int _lhs = 1;
-    private int _rhs = 1;
-
-    public void DisplayResult() {
-        var result = Result();
-        Console.WriteLine($"{_lhs} divided by {_rhs} is {result}");
-    }
-
-    public int Result()
+class Person
+{
+    public string GetName() 
     {
-        int result = _lhs / _rhs;
-        return result;
+        return "JR";
     }
-    public void SetLhsRhs(int lhs, int rhs)
-    {
-        _lhs = lhs;
-        if(rhs == 0)
-        {
-            Console.WriteLine("You cannot divide by zero.");
-            _rhs = 1;
-            _lhs = 1;
-        }
+}
 
-        else
-        {
-            _rhs = rhs;
-        }
-        
-    }
+class Student: Person
+{
 
 }
