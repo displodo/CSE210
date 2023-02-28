792cc5b53a4e45351cef3d37653b4012392d4335
JR Forsyth <for20002@byui.edu>
2023-02-28 15:11:14 -0700

    Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        var shapes = new List<Shape>();
+        var circle = new Circle("blue", 5);
+        var rectangle = new Rectangle("orange", 3, 6);
+        var square = new Square("red", 7);
+        shapes.Add(circle);
+        shapes.Add(rectangle);
+        shapes.Add(square);
+        foreach(var s in shapes)
+        {
+            Console.WriteLine($"Color: {s.GetColor()}");
+            Console.WriteLine($"Area: {s.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
